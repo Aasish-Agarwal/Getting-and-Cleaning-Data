50e425c077ced247553225b6c0cdcd5f37dcc025
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2014-09-20 18:03:32 +0530

    Implemented Download & UnCompress

--- run_analysis.R
+++ run_analysis.R
@@ -58,7 +58,49 @@
   getdata()
 ## ###################################################
   
+## Execution ####################################
+  
+  ## Routines
 
+  ## Merges the training and the test sets to create one data set.
+  step1 <- function() {
+    cat ("Executing Step 1:\n")
+    cat ("\tReading Test Data Set From: \"", datadir , "/test/X_test.txt\"\n" , sep = "")
+    cat ("\tReading Training Data Set From: \"", datadir , "/train/X_train.txt\"\n", sep = "")
+    
+    # datadir
+    # alldata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    # 'train/X_train.txt': Training set.
+    # 'test/X_test.txt': Test set.
+    
+    
+  }
+
+  step2 <- function() {
+    cat ("Executing Step 2:\n")
+  }
+  step3 <- function() {
+    cat ("Executing Step 3:\n")
+  }
+  step4 <- function() {
+    cat ("Executing Step 4:\n")
+  }
+  step5 <- function() {
+    cat ("Executing Step 5:\n")
+  }
+  
+  
+  
+  ## Calls
+  step1()
+  step2()
+  step3()
+  step4()
+  step5()
+  
+## ###################################################
+  
+  
 
 ## FINALIZATION ####################################
   ## Revert to the original working directory
