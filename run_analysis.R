2c99a7b508f50b3ead2f6f1471b578ef761e3032
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2014-09-20 17:26:06 +0530

    Implemented Download & UnCompress

--- run_analysis.R
+++ run_analysis.R
@@ -14,46 +14,56 @@
 
   
   
-## Service Routines
+## Service Routines ###################################################
+  ## Routine to uncompress a file
   uncompress_downloaded_data <- function(zipfile) {
     cmd <- paste(uncompress_util, uncompress_opt, zipfile)
     system(cmd)
   }
   
-  
-  
-## Get currnet working directory
-base_dir <- getwd()
-cat ("Started in directory: " ,getwd(), "\n")
-
-## Create & move to the working directory
-if (!file.exists(work_dir)) {dir.create(work_dir)}
-setwd(work_dir)
-cat ("Changed to directory: " ,getwd() , "\n")
-
-## Download & Un Compress test data set
-getdata <- function() {
-  cat("Function Called: getdata()\n")
-  zipped <- "testdata.zip"
-  if (!file.exists(zipped)) {
+  ## Download & Un Compress test data set
+  getdata <- function() {
+    cat("Function Called: getdata()\n")
+    zipped <- "testdata.zip"
+    if (!file.exists(zipped)) {
       cat("\tDownloding ", zipped , "\n")
       download.file(url, zipped)
+    }
+    
+    if (!file.exists(datadir)) {
+      cat("\tUncompressing ", zipped , "\n")
+      uncompress_downloaded_data(zipped)
+    } else {
+      cat("\tAlready Un Compressed ", zipped , "\n")
+    }
   }
 
-  if (!file.exists(datadir)) {
-    cat("\tUncompressing ", zipped , "\n")
-    uncompress_downloaded_data(zipped)
-  } else {
-    cat("\tAlready Compressed ", zipped , "\n")
-  }
+## ###################################################
   
+## INITIALIZATION ####################################
+  ## Record working directory
+  ## Create & move to working directory
+  ## Download & uncompress data
   
-}
+  ## Record working directory
+  base_dir <- getwd()
+  cat ("Started in directory: " ,getwd(), "\n")
 
-getdata()
+  ## Create & move to the working directory
+  if (!file.exists(work_dir)) {dir.create(work_dir)}
+  setwd(work_dir)
+  cat ("Changed to directory: " ,getwd() , "\n")
+  
+  ## Download & uncompress data
+  getdata()
+## ###################################################
+  
 
 
+## FINALIZATION ####################################
+  ## Revert to the original working directory
 
-## Revert to the original working directory
-setwd(base_dir)
-cat ("Switched back to directory: " ,getwd(), "\n")
+  setwd(base_dir)
+  cat ("Switched back to directory: " ,getwd(), "\n")
+## ###################################################
+  
